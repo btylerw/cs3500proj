a03cfbd3d3cc0d3bbe7f47244a0a57877ec4a633
Tyler <brown.tyler.w@gmail.com>
2023-10-28 08:57:47 -0700

    Added main file

---
+++ main.py
@@ -0,0 +1,17 @@
+################################
+# BEGINNING OF MAIN PROJECT FILE
+# Student 1: Tyler Brown
+# Student 2: Douglas Cerrato
+# Student 3: Stephen Marks
+# Student 4: Raul Velasco
+################################
+
+import pygame
+import sys
+import os
+
+# Function names in checkers.py will likely need to be changed for compatability
+# main function name will absolutely need to be changed, and the call to it will need to be done in here
+#from checkers import *
+
+# Not going to add in more work here for now because I just woke up and am extraordinarily lazy
