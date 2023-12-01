2cb7c7aeb8ce16a2c56a2c7b654c86bdd6f9bcb3
Tyler Brown <brown.tyler.w@gmail.com>
2023-12-01 10:36:21 -0800

    Changed test case / Refactored castling to handle other scenarios Test case now emulates board from perspective of black. Castling now reflects the perspective change.

--- modules/kingmoves.py
+++ modules/kingmoves.py
@@ -19,8 +19,15 @@ def kingMoves(nodePosition, grid):
     moves.append([1,1])
     moves.append([1,-1])
     # These two moves are to allow castling
-    moves.append([0,3])
-    moves.append([0,-4])
+    if (currentPosition.piece.first_move):
+        # Castling moves if White is on bottom
+        if (row == 7 and currentPosition.piece.team == 'White') or (row == 0 and currentPosition.piece.team == 'Black'):
+            moves.append([0,3])
+            moves.append([0,-4])
+        # Castling moves if Black is on bottom
+        elif (row == 7 and currentPosition.piece.team == 'Black') or (row == 0 and currentPosition.piece.team == 'White'):
+            moves.append([0,4])
+            moves.append([0,-3])
 
     for move in moves:
         RowMove, ColMove = move
@@ -29,9 +36,8 @@ def kingMoves(nodePosition, grid):
         if checker(RowMove, row) and checker(ColMove, column):
             # Checks to see if the space is empty for the move
             if(grid[(RowMove + row)][(ColMove + column)].piece == None):
-                if ColMove >= -1 and ColMove <= 1:
                 # Adds the move to the positions list, a list of possible positions
-                    positions.append([RowMove + row, ColMove + column])
+                positions.append((RowMove + row, ColMove + column))
             else:
                 if(grid[RowMove + row][ColMove + column].piece.team == chess.opposite(grid[row][column].piece.team)):
                     # Adds the move if the piece is able to be taken
@@ -40,15 +46,25 @@ def kingMoves(nodePosition, grid):
                 elif(grid[RowMove+row][ColMove+column].piece.role == 'rook') and currentPosition.piece.first_move and\
                     grid[RowMove+row][ColMove+column].piece.first_move:
                     # If that king and rook have not moved, we're going to check the direction that the castling will take place
-                    if ColMove > 0:
-                        # We can castle to the right
-                        if grid[RowMove+row][column+1].piece == None and grid[RowMove+row][column+2].piece == None:
-                            positions.append([RowMove+row, ColMove+column])
-                    elif ColMove < 0:
-                        # We can castle to the left
-                        if grid[RowMove+row][column-1].piece == None and grid[RowMove+row][column-2].piece == None and\
-                            grid[RowMove+row][column-3].piece == None:
-                            positions.append([RowMove+row, ColMove+column])
+                    match ColMove:
+                        # Castles to the left if White is on bottom
+                        case -4:
+                            if grid[RowMove+row][column-1].piece == None and grid[RowMove+row][column-2].piece == None and\
+                                grid[RowMove+row][column-3].piece == None:
+                                positions.append((RowMove+row, ColMove+column))
+                        # Castles to the left if Black is on bottom
+                        case -3:
+                            if grid[RowMove+row][column-1].piece == None and grid[RowMove+row][column-2].piece == None:
+                                positions.append((RowMove+row, ColMove+column))
+                        # Castles to the right if White is on bottom
+                        case 3:
+                                if grid[RowMove+row][column+1].piece == None and grid[RowMove+row][column+2].piece == None:
+                                    positions.append((RowMove+row, ColMove+column))
+                        # Castles to the right is Black is on bottom
+                        case 4:                            
+                            if grid[RowMove+row][column+1].piece == None and grid[RowMove+row][column+2].piece == None and\
+                                grid[RowMove+row][column+3].piece == None:
+                                positions.append((RowMove+row, ColMove+column))
 
 
     return positions
